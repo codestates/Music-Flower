50d578e71b6023ba93532bad46b0cc919b2fef16
olt13mm <olt13mm@gmail.com>
2022-03-03 15:18:32 +0900

    Feat: 음악 검색 및 playList 저장 컴포넌트 완성

--- client/src/components/SpotifyDashboard.js
+++ client/src/components/SpotifyDashboard.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import spotifyAuth from "../auth/useSpotifyAuth";
 import SpotifyWebApi from "spotify-web-api-node";
+import TrackSearchResult from "./TrackSerchResult";
+import SavePlayList from "./SavePlayList";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: "26044bd61ea049d4a54e2798864d988b",
@@ -11,28 +13,77 @@ export default function SpDashboard({ code }) {
   // console.log("accesstoken", accessToken);
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
 
-  const playlist = [];
+  console.log("searchResult", searchResult);
+  console.log("playList", playlist);
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    setPlaylist([...playlist, track]);
+    setSearch("");
+  }
 
   useEffect(() => {
+    console.log("accessToek");
     if (!accessToken) return;
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken, playlist]);
 
   useEffect(() => {
+    console.log("use Effect 트랙가져오기");
     if (!search) return setSearchResult([]);
     if (!accessToken) return;
 
-    // let cancel = false;
+    let cancel = false;
     spotifyApi.searchTracks(search).then((res) => {
-      console.log("res");
+      console.log("cancel", cancel);
+      if (cancel) return;
+      console.log(res.body.tracks.items);
+      setSearchResult(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
     });
+    return () => (cancel = true);
   }, [search, accessToken]);
 
   return (
     <div>
-      <input value={search} onChange={(e) => setSearch(e.target.value)}></input>
-      {code}
+      <div>
+        {playlist.map((track) => (
+          <SavePlayList track={track} key={track.uri} />
+        ))}
+      </div>
+      <div>
+        <input
+          type="search"
+          name="q"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+      </div>
+      {searchResult.map((track) => (
+        <TrackSearchResult
+          track={track}
+          key={track.uri}
+          chooseTrack={chooseTrack}
+        />
+      ))}
     </div>
   );
 }
