87823327a691223f16eda5a70c58c693d41db778
jiyuunyang <jiyuunyang@gmail.com>
2022-03-04 16:34:08 +0900

    Feat: 모델 생성, 마이그레이션 진행

---
+++ server/migrations/20220304064133-create-like.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      postId: {
+        type: Sequelize.INTEGER
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Likes');
+  }
+};
---
+++ server/models/post.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Post.init({
+    userId: DataTypes.INTEGER,
+    postTitle: DataTypes.STRING,
+    image: DataTypes.STRING,
+    postExplain: DataTypes.STRING,
+    totalLike: DataTypes.INTEGER,
+    totalComment: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Post',
+  });
+  return Post;
+};
