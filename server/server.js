814bca2a60d996365bdcb290f2eb6240af8eef93
olt13mm <olt13mm@gmail.com>
2022-03-08 09:47:55 +0900

    Dev: save

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ const spotifyRouter = require("./router/spotifyRouter");
 
 sequelize
   .sync({ force: false })
-  .then(() => {
-    console.log("데이터베이스 연결");
+  .then((e) => {
+    console.log("데이터베이스 연결", e);
   })
   .catch((err) => {
     console.error(err);
